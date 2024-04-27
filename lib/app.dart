cfefdaa4f9f507971613d85024df5aff5d84d941
Leo Wong <gnowoel@gmail.com>
2024-04-27 10:38:25 +0800

    Split `main.dart`

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}

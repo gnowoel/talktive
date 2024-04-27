644165bb2fe06afcf74c6fc54d08cdb5e4ce4780
Leo Wong <gnowoel@gmail.com>
2024-04-27 19:25:31 +0800

    Display a "Sign In" button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,9 +5,16 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Firebase initialized.'),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: FilledButton(
+            onPressed: () {
+              print('Sign In button pressed.');
+            },
+            child: const Text('Sign In'),
+          ),
+        ),
       ),
     );
   }

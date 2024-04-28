2811d7ce3b9cab1fbb6fc983b4647c21f1d931fb
Leo Wong <gnowoel@gmail.com>
2024-04-28 15:40:27 +0800

    Sign in automatically

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.signInAnonymously();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +25,12 @@ class Home extends StatelessWidget {
           body: SafeArea(
             child: Center(
               child: currentUser == null
-                  ? buildSignInButton()
+                  ? const CircularProgressIndicator()
                   : buildUserProfile(currentUser),
             ),
           ),
         );
       },
-    );
-  }
-
-  Widget buildSignInButton() {
-    return FilledButton(
-      onPressed: () {
-        FirebaseAuth.instance.signInAnonymously();
-      },
-      child: const Text('Sign In'),
     );
   }
 

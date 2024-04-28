8b29f70f94e8e42af667b921e4d746481e0e241e
Leo Wong <gnowoel@gmail.com>
2024-04-28 18:52:24 +0800

    Pick a random emoji as the avatar

--- lib/pages/user.dart
+++ lib/pages/user.dart
@@ -1,20 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:unicode_emojis/unicode_emojis.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const List<Emoji> emojis = UnicodeEmojis.allEmojis;
+    Emoji emoji = emojis[Random().nextInt(emojis.length)];
+
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                '\u{1f44c}',
-                style: TextStyle(fontSize: 64),
+              Text(
+                emoji.emoji,
+                style: const TextStyle(fontSize: 64),
               ),
+              Text(emoji.name),
               const SizedBox(height: 25),
               FilledButton(
                 onPressed: () {},

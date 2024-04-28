7b2ffc62d410ea2520d48621df337582cfe1dbc2
Leo Wong <gnowoel@gmail.com>
2024-04-28 10:52:13 +0800

    Convert Home to StatefulWidget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(

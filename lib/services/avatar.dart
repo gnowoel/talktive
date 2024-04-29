1e15e23bd008b07c9244e9ef29cbfa58fe2ab3ef
Leo Wong <gnowoel@gmail.com>
2024-04-29 15:10:04 +0800

    Filter out emojis

--- lib/services/avatar.dart
+++ lib/services/avatar.dart
@@ -6,11 +6,24 @@ import 'package:unicode_emojis/unicode_emojis.dart';
 class Avatar extends ChangeNotifier {
   Emoji current = _randomEmoji();
 
+  static Emoji _randomEmoji() {
+    return _selectedEmojis[Random().nextInt(_selectedEmojis.length)];
+  }
+
+  static final List<Emoji> _selectedEmojis = _allEmojis
+      .where((emoji) => _selectedCategories.contains(emoji.category))
+      .toList();
+
   static const _allEmojis = UnicodeEmojis.allEmojis;
 
-  static Emoji _randomEmoji() {
-    return _allEmojis[Random().nextInt(_allEmojis.length)];
-  }
+  static const List<Category> _selectedCategories = [
+    Category.peopleAndBody,
+    Category.animalsAndNature,
+    Category.foodAndDrink,
+    Category.travelAndPlaces,
+    Category.activities,
+    Category.objects,
+  ];
 
   void refresh() {
     current = _randomEmoji();

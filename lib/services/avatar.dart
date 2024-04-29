32ecd28e412c835e5ced9ea7e29ad9b7af22736e
Leo Wong <gnowoel@gmail.com>
2024-04-29 10:09:49 +0800

    Apply singleton pattern to AvatarService

--- lib/services/avatar.dart
+++ lib/services/avatar.dart
@@ -5,9 +5,15 @@ import 'package:unicode_emojis/unicode_emojis.dart';
 class AvatarService {
   Emoji current = _randomEmoji();
 
+  AvatarService._();
+
+  static final AvatarService _instance = AvatarService._();
+
   static const _allEmojis = UnicodeEmojis.allEmojis;
 
   static Emoji _randomEmoji() {
     return _allEmojis[Random().nextInt(_allEmojis.length)];
   }
+
+  factory AvatarService() => _instance;
 }

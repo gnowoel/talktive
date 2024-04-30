743a7de05bda62fb3da84600b165674a779fbb63
Leo Wong <gnowoel@gmail.com>
2024-04-30 13:30:54 +0800

    Provide the `Fireauth` object

--- lib/app.dart
+++ lib/app.dart
@@ -3,14 +3,18 @@ import 'package:provider/provider.dart';
 
 import 'home.dart';
 import 'services/avatar.dart';
+import 'services/fireauth.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Avatar(),
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => Fireauth()),
+        ChangeNotifierProvider(create: (context) => Avatar()),
+      ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Talktive',

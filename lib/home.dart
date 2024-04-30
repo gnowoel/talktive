40600e6fb503a0da1ab5f41e24dda019e5dc7c35
Leo Wong <gnowoel@gmail.com>
2024-04-30 13:39:57 +0800

    Use the provided `Fireauth` object in `Home`

--- lib/home.dart
+++ lib/home.dart
@@ -1,26 +1,33 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/empty.dart';
 import 'pages/error.dart';
 import 'pages/user.dart';
+import 'services/fireauth.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: FirebaseAuth.instance.signInAnonymously(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return ErrorPage(message: '${snapshot.error}');
-        } else if (!snapshot.hasData) {
-          return const EmptyPage();
-        } else {
-          return const UserPage();
-        }
-      },
-    );
+    final fireauth = Provider.of<Fireauth>(context);
+
+    try {
+      return FutureBuilder(
+        future: fireauth.signInAnonymously(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorPage(message: '${snapshot.error}');
+          } else if (!snapshot.hasData) {
+            return const EmptyPage();
+          } else {
+            return const UserPage();
+          }
+        },
+      );
+    } catch (e) {
+      return ErrorPage(message: '$e');
+    }
   }
 }

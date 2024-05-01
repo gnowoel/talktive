c959e9b554a0c3bf43f2a430a2b7138621481602
Leo Wong <gnowoel@gmail.com>
2024-05-01 19:51:52 +0800

    Add a "Retry" button in the error page

--- lib/home.dart
+++ lib/home.dart
@@ -6,8 +6,21 @@ import 'pages/error.dart';
 import 'pages/user.dart';
 import 'services/fireauth.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Key futureBuilderKey = UniqueKey();
+
+  void refresh() {
+    setState(() {
+      futureBuilderKey = UniqueKey();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +28,14 @@ class Home extends StatelessWidget {
 
     try {
       return FutureBuilder(
+        key: futureBuilderKey,
         future: fireauth.signInAnonymously(),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            return ErrorPage(message: '${snapshot.error}');
+            return ErrorPage(
+              message: '${snapshot.error}',
+              refresh: refresh,
+            );
           } else if (!snapshot.hasData) {
             return const EmptyPage();
           } else {
@@ -27,7 +44,10 @@ class Home extends StatelessWidget {
         },
       );
     } catch (e) {
-      return ErrorPage(message: '$e');
+      return ErrorPage(
+        message: '$e',
+        refresh: refresh,
+      );
     }
   }
 }
--- lib/pages/error.dart
+++ lib/pages/error.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 class ErrorPage extends StatefulWidget {
-  const ErrorPage({super.key, required this.message});
+  const ErrorPage({
+    super.key,
+    required this.message,
+    required this.refresh,
+  });
 
   final String message;
+  final VoidCallback refresh;
 
   @override
   State<ErrorPage> createState() => _ErrorPageState();
@@ -26,12 +31,21 @@ class _ErrorPageState extends State<ErrorPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text(
-            '\u{1f641}',
-            style: TextStyle(fontSize: 64),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '\u{1f641}',
+                style: TextStyle(fontSize: 64),
+              ),
+              TextButton(
+                onPressed: widget.refresh,
+                child: const Text('Retry'),
+              ),
+            ],
           ),
         ),
       ),

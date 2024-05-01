e3547fa86fa5fe6c9a962afbfc7e58ad24307c8f
Leo Wong <gnowoel@gmail.com>
2024-05-01 16:27:27 +0800

    Show a snackbar in the error page

--- lib/pages/error.dart
+++ lib/pages/error.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
-class ErrorPage extends StatelessWidget {
+class ErrorPage extends StatefulWidget {
   const ErrorPage({super.key, required this.message});
 
   final String message;
 
   @override
+  State<ErrorPage> createState() => _ErrorPageState();
+}
+
+class _ErrorPageState extends State<ErrorPage> {
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(widget.message),
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                '\u{1f641}',
-                style: TextStyle(fontSize: 64),
-              ),
-              Text(message),
-            ],
+          child: Text(
+            '\u{1f641}',
+            style: TextStyle(fontSize: 64),
           ),
         ),
       ),

67f06efbdd746bf6aecb20bcc9ed1f08827600fe
Leo Wong <gnowoel@gmail.com>
2024-05-06 16:13:35 +0800

    Implement `Firedata.createRoom()`

---
+++ lib/services/firedata.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_database/firebase_database.dart';
+
+import '../models/room.dart';
+
+class Firedata {
+  final FirebaseDatabase instance = FirebaseDatabase.instance;
+
+  Future<Room> createRoom(userId, userName, userCode, languageCode) async {
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+
+    final room = Room(
+      userId: userId,
+      userName: userName,
+      userCode: userCode,
+      languageCode: languageCode,
+      createdAt: timestamp,
+      updatedAt: 0,
+    );
+
+    final ref = instance.ref('rooms').push();
+
+    await ref.set(room.toJson());
+
+    return room..id = ref.key;
+  }
+}

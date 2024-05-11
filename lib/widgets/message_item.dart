9882cf223810322cc38a720a833ae8c7429b805d
Leo Wong <gnowoel@gmail.com>
2024-05-11 22:22:29 +0800

    Align one's own messages to the right

--- lib/widgets/message_item.dart
+++ lib/widgets/message_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../models/message.dart';
+import '../services/fireauth.dart';
 import 'bubble.dart';
 
 class MessageItem extends StatelessWidget {
@@ -10,6 +12,14 @@ class MessageItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final fireauth = Provider.of<Fireauth>(context, listen: false);
+    final currentUser = fireauth.instance.currentUser!;
+    final byMe = message.userId == currentUser.uid;
+
+    return byMe ? _buildMessageItemRight() : _buildMessageItemLeft();
+  }
+
+  Widget _buildMessageItemLeft() {
     return Padding(
       padding: const EdgeInsets.all(8),
       child: Row(
@@ -20,7 +30,44 @@ class MessageItem extends StatelessWidget {
             style: const TextStyle(fontSize: 24),
           ),
           const SizedBox(width: 8),
-          Bubble(content: message.content),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Bubble(content: message.content),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 48),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageItemRight() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const SizedBox(width: 48),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Flexible(
+                  child: Bubble(content: message.content),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 8),
+          Text(
+            message.userCode,
+            style: const TextStyle(fontSize: 24),
+          ),
         ],
       ),
     );

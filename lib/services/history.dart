20a1dea42934d0e5969e1e0b69203dc40dd48d69
Leo Wong <gnowoel@gmail.com>
2024-05-13 11:39:36 +0800

    Save history records in shared preferences

--- lib/services/history.dart
+++ lib/services/history.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import '../models/record.dart';
 import '../models/room.dart';
+import 'prefs.dart';
 
 class History {
+  final prefs = Prefs();
   final records = <Record>[];
 
   Future<void> saveRecord(Room room) async {
@@ -15,5 +19,7 @@ class History {
 
     records.removeWhere((element) => element.roomId == record.roomId);
     records.add(record);
+
+    await prefs.setString('records', jsonEncode(records));
   }
 }

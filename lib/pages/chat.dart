9610d06e1feb08683532cff3a6a9392f4141e057
Leo Wong <gnowoel@gmail.com>
2024-05-13 08:56:27 +0800

    Save recent room records

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../models/room.dart';
+import '../services/history.dart';
 import '../widgets/input_field.dart';
 import '../widgets/message_list.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   final Room room;
 
   const ChatPage({
@@ -13,19 +15,43 @@ class ChatPage extends StatelessWidget {
   });
 
   @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  late History history;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    history = Provider.of<History>(context, listen: false);
+    _addHistoryRecord();
+  }
+
+  @override
+  void dispose() {
+    _addHistoryRecord();
+    super.dispose();
+  }
+
+  Future<void> _addHistoryRecord() async {
+    await history.saveRecord(widget.room);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text(room.userName),
+        title: Text(widget.room.userName),
       ),
       body: SafeArea(
         child: Column(
           children: [
             Expanded(
-              child: MessageList(room: room),
+              child: MessageList(room: widget.room),
             ),
-            InputField(room: room),
+            InputField(room: widget.room),
           ],
         ),
       ),
--- lib/services/history.dart
+++ lib/services/history.dart
@@ -1,9 +1,18 @@
 import '../models/record.dart';
+import '../models/room.dart';
 
 class History {
   final records = <Record>[];
 
-  Future<void> saveRecord(Record record) async {
+  Future<void> saveRecord(Room room) async {
+    final record = Record(
+      roomId: room.id,
+      roomUserId: room.userId,
+      roomUserName: room.userName,
+      roomUserCode: room.userCode,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+    );
+
     records.removeWhere((element) => element.roomId == record.roomId);
     records.add(record);
   }

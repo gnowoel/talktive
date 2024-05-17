8cf5964cca79da639d49901dffd3015873944ad5
Leo Wong <gnowoel@gmail.com>
2024-05-17 18:27:21 +0800

    Update the health bar every 10 minutes

--- lib/widgets/health.dart
+++ lib/widgets/health.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import 'heart_list.dart';
 
-class Health extends StatelessWidget {
+class Health extends StatefulWidget {
   final int roomUpdatedAt;
 
   const Health({
@@ -11,11 +12,40 @@ class Health extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final now = DateTime.now();
-    final then = DateTime.fromMillisecondsSinceEpoch(roomUpdatedAt);
-    final elapsed = now.difference(then);
+  State<Health> createState() => _HealthState();
+}
 
+class _HealthState extends State<Health> with SingleTickerProviderStateMixin {
+  late Ticker ticker;
+  late Duration elapsed;
+
+  @override
+  void initState() {
+    super.initState();
+    elapsed = _getElapsed();
+    ticker = createTicker((_) {
+      final newElapsed = _getElapsed();
+      if (newElapsed.inMinutes % 10 == 0) {
+        setState(() => elapsed = newElapsed);
+      }
+    });
+    ticker.start();
+  }
+
+  @override
+  void dispose() {
+    ticker.dispose();
+    super.dispose();
+  }
+
+  Duration _getElapsed() {
+    final now = DateTime.now();
+    final then = DateTime.fromMillisecondsSinceEpoch(widget.roomUpdatedAt);
+    return now.difference(then);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return HeartList(elapsed: elapsed);
   }
 }

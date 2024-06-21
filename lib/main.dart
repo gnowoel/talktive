895b7b3449f1b8af988504d5407e05bfc67aece7
Leo Wong <gnowoel@gmail.com>
2024-06-21 20:49:54 +0800

    Use Firebase Emulator Suite
    Turns out that Anonymous Authentication only works for Web apps.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -11,6 +14,20 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kDebugMode) {
+    final host = defaultTargetPlatform == TargetPlatform.android
+        ? '10.0.2.2'
+        : 'localhost';
+
+    try {
+      await FirebaseAuth.instance.useAuthEmulator(host, 9099);
+      FirebaseDatabase.instance.useDatabaseEmulator(host, 9000);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
   await History().loadRecords();
   Firedata().syncTime();
   runApp(const App());

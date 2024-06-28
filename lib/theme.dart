b4709e98ca7d4438e40e1bbdf4888a58a908e9d1
Leo Wong <gnowoel@gmail.com>
2024-06-28 16:55:20 +0800

    Update colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
-    seedColor: Colors.deepPurple,
+    seedColor: Colors.lightGreen.shade400,
   ),
-  scaffoldBackgroundColor: Colors.white,
+  // scaffoldBackgroundColor: Colors.white,
 );
--- lib/widgets/heart_item.dart
+++ lib/widgets/heart_item.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 
 class HeartItem extends StatelessWidget {
   final IconData icon;
-  final MaterialColor color;
+  final MaterialColor? color;
 
   const HeartItem({
     super.key,
     this.icon = Icons.favorite,
-    this.color = Colors.pink,
+    this.color,
   });
 
   @override
   Widget build(BuildContext context) {
     return Icon(
       icon,
-      color: color,
+      color: color ?? Colors.pink.shade400,
       size: 20,
     );
   }

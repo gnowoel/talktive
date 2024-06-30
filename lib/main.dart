5b410be1087367add12a48a8fba01c0b3e6b2917
Leo Wong <gnowoel@gmail.com>
2024-06-30 15:48:15 +0800

    Comment out the unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:firebase_database/firebase_database.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';

67cb74047b045c74721087183881d4cb28e4aff2
Leo Wong <gnowoel@gmail.com>
2024-06-30 23:26:15 +0800

    Test run the first Cloud Function on localhost

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,20 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
+const { onValueCreated } = require("firebase-functions/v2/database");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+exports.touch = onValueCreated("/messages/{roomId}/*", (event) => {
+  const original = event.data.val();
+  logger.info(event.params.roomId);
+  logger.info(original);
+});

95b6e5369b6ba330ee7623024724405f1ed2000b
Leo Wong <gnowoel@gmail.com>
2024-07-01 10:10:20 +0800

    Add ESLint support for functions

--- functions/index.js
+++ functions/index.js
@@ -7,20 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const { onValueCreated } = require("firebase-functions/v2/database");
+const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
-
-exports.touch = onValueCreated("/messages/{roomId}/*", (event) => {
-  const original = event.data.val();
-  logger.info(event.params.roomId);
-  logger.info(original);
-});
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });

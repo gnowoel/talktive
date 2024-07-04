212023899f1c631f8560aa1a33fd18156fce6b58
Leo Wong <gnowoel@gmail.com>
2024-07-04 13:07:48 +0800

    Add an http function for triggering schedule

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,15 @@
 const updateRoomTimestamp = require("./src/updateRoomTimstamp");
 const markRoomsExpired = require("./src/markRoomsExpired");
 const updateRoomFilter = require("./src/updateRoomFilter");
-const scheduledCleanup = require("./src/scheduledCleanup");
+const {
+  requestedCleanup,
+  scheduledCleanup,
+} = require("./src/scheduledCleanup");
 
 module.exports = {
   updateRoomTimestamp,
   markRoomsExpired,
   updateRoomFilter,
+  requestedCleanup,
   scheduledCleanup,
 };
--- functions/src/scheduledCleanup.js
+++ functions/src/scheduledCleanup.js
@@ -1,8 +1,41 @@
+const { onRequest } = require("firebase-functions/v2/https");
 const { onSchedule } = require("firebase-functions/v2/scheduler");
 const { logger } = require("firebase-functions");
+const admin = require("firebase-admin");
 
-const scheduledCleanup = onSchedule("every hour", async (event) => {
-  logger.info(event);
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+const db = admin.database();
+
+const requestedCleanup = onRequest((req, res) => {
+  cleanup()
+    .then(() => {
+      res.send("success");
+    })
+    .catch((error) => {
+      logger.error(error);
+    });
 });
 
-module.exports = scheduledCleanup;
+const scheduledCleanup = onSchedule("every hour", async (event) => {
+  cleanup().catch((error) => {
+    logger.error(error);
+  });
+});
+
+const cleanup = () => {
+  const ref = db.ref(`test/abc`);
+  const promise = ref.set({ id: `${randomNumber(100, 999)}` });
+  return promise;
+};
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+module.exports = {
+  requestedCleanup,
+  scheduledCleanup,
+};

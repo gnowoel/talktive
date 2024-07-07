93afa1f9780af427ebfd833788a37ebfe7adcff6
Leo Wong <gnowoel@gmail.com>
2024-07-07 16:17:58 +0800

    Use anonymous authentication on server for now

--- lib/services/fireauth.dart
+++ lib/services/fireauth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 
 import '../helpers/helpers.dart';
 
@@ -13,16 +14,16 @@ class Fireauth {
     }
 
     try {
-      // We can also use Firebase Anonymous Authentication
-      // if (!kDebugMode) {
-      //   final userCredential = await instance.signInAnonymously();
-      //   return userCredential.user!;
-      // }
-      final userCredential = await instance.createUserWithEmailAndPassword(
-        email: generateEmail(),
-        password: generatePassword(),
-      );
-      return userCredential.user!;
+      if (kDebugMode) {
+        final userCredential = await instance.createUserWithEmailAndPassword(
+          email: generateEmail(),
+          password: generatePassword(),
+        );
+        return userCredential.user!;
+      } else {
+        final userCredential = await instance.signInAnonymously();
+        return userCredential.user!;
+      }
     } on FirebaseAuthException catch (e) {
       // TODO: logout unless there's a network error
       // await instance.signOut();

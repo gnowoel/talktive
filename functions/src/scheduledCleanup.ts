79309828557ff0f8be17065a21c265039b49cb53
Leo Wong <gnowoel@gmail.com>
2024-07-10 23:18:30 +0800

    Replace promises with async/await in scheduledCleanup

--- functions/src/scheduledCleanup.ts
+++ functions/src/scheduledCleanup.ts
@@ -22,102 +22,106 @@ interface Params {
   [userId: string]: null
 }
 
-interface Room {
-  id: string
-}
-
 export const scheduledCleanup = onSchedule('every hour', async (_) => {
-  cleanup().catch((error) => {
+  try {
+    await cleanup();
+  } catch (error) {
     logger.error(error);
-  });
+  }
 });
 
-export const requestedCleanup = onRequest((req, res) => {
-  cleanup()
-    .catch((error) => {
-      logger.error(error);
-    })
-    .then(() => {
-      res.send('success');
-    });
+export const requestedCleanup = onRequest(async (req, res) => {
+  try {
+    await cleanup();
+  } catch (error) {
+    logger.error(error);
+  }
+
+  res.send('success');
 });
 
 const cleanup = async () => {
-  return Promise.resolve()
-    .then(() => cleanupUsers())
-    .then(() => cleanupRooms());
+  try {
+    await cleanupUsers();
+    await cleanupRooms();
+  } catch (error) {
+    logger.error(error);
+  }
 };
 
-const cleanupUsers = () => {
+const cleanupUsers = async () => {
   const ref = db.ref('users');
   const time = new Date((new Date().getTime() - priorUserDeleting)).toJSON();
+
   const query = ref
     .orderByChild('filter')
     .startAt('temp-0000')
     .endAt(`temp-${time}`)
     .limitToFirst(1000);
 
-  return query
-    .get()
-    .then((snapshot) => {
-      if (!snapshot.exists()) return [];
-      return snapshot.val();
-    })
-    .then((users) => {
-      const userIds = Object.keys(users);
-      const params: Params = {};
-      const usersRef = db.ref('users');
+  const snapshot = await query.get();
 
-      userIds.forEach((userId) => {
-        params[userId] = null;
-      });
+  if (!snapshot.exists()) return;
 
-      return getAuth().deleteUsers(userIds).then(() => {
-        usersRef.update(params);
-      });
-    });
+  const users = snapshot.val();
+  const userIds = Object.keys(users);
+  const params: Params = {};
+  const usersRef = db.ref('users');
+
+  userIds.forEach((userId) => {
+    params[userId] = null;
+  });
+
+  try {
+    await getAuth().deleteUsers(userIds);
+    await usersRef.update(params);
+  } catch (error) {
+    logger.error(error);
+  }
 };
 
-const cleanupRooms = () => {
+const cleanupRooms = async () => {
   const ref = db.ref('rooms');
   const time = new Date().getTime() - priorRoomDeleting;
   const query = ref.orderByChild('closedAt').endAt(time);
 
-  return query
-    .get()
-    .then((snapshot) => {
-      if (!snapshot.exists()) return []; // for empty results
-      const rooms = snapshot.val();
-      return Object.keys(rooms).map((roomId) => {
-        return {
-          id: roomId,
-          ...rooms[roomId],
-        };
-      });
-    })
-    .then((rooms) => {
-      return rooms.reduce((p, room) => {
-        return p
-          .then(() => {
-            return markRoomDeleted(room);
-          })
-          .then(() => {
-            return removeMessages(room);
-          });
-      }, Promise.resolve());
-    });
+  const snapshot = await query.get();
+
+  if (!snapshot.exists()) return;
+
+  const rooms = snapshot.val();
+  const roomIds = Object.keys(rooms);
+
+  try {
+    for (const roomId in roomIds) {
+      await markRoomDeleted(roomId);
+      await removeMessages(roomId);
+    }
+  } catch (error) {
+    logger.error(error);
+  }
 };
 
-const markRoomDeleted = (room: Room) => {
-  const roomRef = db.ref(`rooms/${room.id}`);
+const markRoomDeleted = async (roomId: string) => {
+  const roomRef = db.ref(`rooms/${roomId}`);
   const filterZ = '-zzzz';
-  return roomRef.update({
-    filter: filterZ,
-    deletedAt: new Date().getTime(),
-  });
+
+  try {
+    await roomRef.update({
+      filter: filterZ,
+      deletedAt: new Date().getTime(),
+    });
+  } catch (error) {
+    logger.error(error);
+  }
 };
 
-const removeMessages = (room: Room) => {
-  const messagesRef = db.ref(`messages/${room.id}`);
-  return messagesRef.remove();
+const removeMessages = async (roomId: string) => {
+  const messagesRef = db.ref(`messages/${roomId}`);
+
+  try {
+    await messagesRef.remove();
+  } catch (error) {
+    logger.error(error);
+  }
 };

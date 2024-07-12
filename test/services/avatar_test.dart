ae7ff2e8d20e6c032ae8dc5186dff8905af4c7c3
Leo Wong <gnowoel@gmail.com>
2024-07-12 23:23:56 +0800

    Test the `Avatar` service

---
+++ test/services/avatar_test.dart
@@ -0,0 +1,27 @@
+import 'package:test/test.dart';
+import 'package:talktive/services/avatar.dart';
+
+void main() {
+  group('Avatar', () {
+    final avatar = Avatar();
+
+    test('has an initial value', () {
+      expect(avatar.name.isNotEmpty, true);
+      expect(avatar.code.isNotEmpty, true);
+    });
+
+    test('can generate new values', () {
+      avatar.refresh();
+      expect(avatar.name.isNotEmpty, true);
+      expect(avatar.code.isNotEmpty, true);
+    });
+
+    test('can change old values', () {
+      final name = avatar.name;
+      final code = avatar.code;
+      avatar.refresh();
+      expect(avatar.name == name, false);
+      expect(avatar.code == code, false);
+    });
+  });
+}

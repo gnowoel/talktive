48da257df96bb6506aa9458e83cd6f358e82e72f
Leo Wong <gnowoel@gmail.com>
2024-07-17 18:21:18 +0800

    Check message sending in the integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,19 +1,35 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:talktive/app.dart';
+import 'package:talktive/widgets/bubble.dart';
 
 import 'setup.dart';
 
 Future<void> main() async {
   await setup();
 
-  group('app', () {
-    testWidgets('Start the app', (tester) async {
+  group('Talktive', () {
+    testWidgets('walkthrough', (tester) async {
       await tester.pumpWidget(const App());
-
       await tester.pumpAndSettle();
 
-      expect(find.text('Read'), findsOneWidget);
-      expect(find.text('Write'), findsOneWidget);
+      final writeButtonFinder = find.widgetWithText(OutlinedButton, 'Write');
+      expect(writeButtonFinder, findsOneWidget);
+
+      await tester.tap(writeButtonFinder);
+      await tester.pump(const Duration(seconds: 2));
+
+      final textFieldFinder = find.byType(TextField);
+      final sendButtonFinder = find.byIcon(Icons.send);
+      expect(textFieldFinder, findsOneWidget);
+      expect(sendButtonFinder, findsOneWidget);
+
+      await tester.enterText(textFieldFinder, 'Hi!');
+      await tester.tap(sendButtonFinder);
+      await tester.pump(const Duration(seconds: 2));
+
+      final bubbleFinder = find.widgetWithText(Bubble, 'Hi!');
+      expect(bubbleFinder, findsOneWidget);
     });
   });
 }

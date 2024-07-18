5a0fa3ad292d28b32ad9b77f1a940f3d8c49e727
Leo Wong <gnowoel@gmail.com>
2024-07-18 10:26:49 +0800

    Expand the integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -8,53 +8,84 @@ import 'setup.dart';
 Future<void> main() async {
   await setup();
 
-  testWidgets('App', (tester) async {
-    await tester.pumpWidget(const App());
-    await tester.pumpAndSettle();
+  final messageContent = DateTime.now().toIso8601String();
 
-    // Write
+  group('App', () {
+    testWidgets('walkthrough of Write, History & Read', (tester) async {
+      await tester.pumpWidget(const App());
+      await tester.pumpAndSettle();
 
-    final writeButtonFinder = find.widgetWithText(OutlinedButton, 'Write');
-    expect(writeButtonFinder, findsOneWidget);
+      // Write
 
-    await tester.tap(writeButtonFinder);
-    await tester.pump(const Duration(seconds: 2));
+      final writeButtonFinder = find.widgetWithText(OutlinedButton, 'Write');
+      expect(writeButtonFinder, findsOneWidget);
 
-    final textFieldFinder = find.byType(TextField);
-    final sendButtonFinder = find.byIcon(Icons.send);
-    expect(textFieldFinder, findsOneWidget);
-    expect(sendButtonFinder, findsOneWidget);
+      await tester.tap(writeButtonFinder);
+      await tester.pump(const Duration(seconds: 5));
 
-    await tester.enterText(textFieldFinder, 'Hi!');
-    await tester.tap(sendButtonFinder);
-    await tester.pump(const Duration(seconds: 2));
+      final textFieldFinder = find.byType(TextField);
+      final sendButtonFinder = find.byIcon(Icons.send);
+      expect(textFieldFinder, findsOneWidget);
+      expect(sendButtonFinder, findsOneWidget);
 
-    final bubbleFinder1 = find.widgetWithText(Bubble, 'Hi!');
-    expect(bubbleFinder1, findsOneWidget);
+      await tester.enterText(textFieldFinder, messageContent);
+      await tester.tap(sendButtonFinder);
+      await tester.pump(const Duration(seconds: 5));
 
-    final backButtonFinder = find.byType(BackButton);
-    expect(backButtonFinder, findsOneWidget);
+      final bubbleFinder1 = find.widgetWithText(Bubble, messageContent);
+      expect(bubbleFinder1, findsOneWidget);
 
-    await tester.tap(backButtonFinder);
-    await tester.pump(const Duration(seconds: 2));
+      final backButtonFinder1 = find.byType(BackButton);
+      expect(backButtonFinder1, findsOneWidget);
 
-    // Read
+      await tester.tap(backButtonFinder1);
+      await tester.pump(const Duration(seconds: 5));
 
-    final historyButtonFinder = find.byIcon(Icons.history);
-    expect(historyButtonFinder, findsOneWidget);
+      // History
 
-    await tester.tap(historyButtonFinder);
-    await tester.pump(const Duration(seconds: 2));
+      final historyButtonFinder = find.byIcon(Icons.history);
+      expect(historyButtonFinder, findsOneWidget);
 
-    final listTileFinder = find.byType(ListTile);
-    expect(listTileFinder, findsWidgets);
+      await tester.tap(historyButtonFinder);
+      await tester.pump(const Duration(seconds: 5));
 
-    await tester.tap(listTileFinder.first);
-    await tester.pump(const Duration(seconds: 2));
+      final listTileFinder = find.byType(ListTile);
+      expect(listTileFinder, findsWidgets);
 
-    final bubbleFinder2 = find.widgetWithText(Bubble, 'Hi!');
-    expect(bubbleFinder2, findsOneWidget);
+      await tester.tap(listTileFinder.first);
+      await tester.pump(const Duration(seconds: 5));
 
-    await tester.pump(const Duration(seconds: 2));
+      final bubbleFinder2 = find.widgetWithText(Bubble, messageContent);
+      expect(bubbleFinder2, findsOneWidget);
+
+      final backButtonFinder2 = find.byType(BackButton);
+      expect(backButtonFinder2, findsOneWidget);
+
+      await tester.tap(backButtonFinder2);
+      await tester.pump(const Duration(seconds: 5));
+
+      final backButtonFinder3 = find.byType(BackButton);
+      expect(backButtonFinder3, findsOneWidget);
+
+      await tester.tap(backButtonFinder3);
+      await tester.pump(const Duration(seconds: 5));
+
+      // Read
+
+      final readButtonFinder = find.widgetWithText(FilledButton, 'Read');
+      expect(readButtonFinder, findsOneWidget);
+
+      await tester.tap(readButtonFinder);
+      await tester.pump(const Duration(seconds: 5));
+
+      final bubbleFinder3 = find.widgetWithText(Bubble, messageContent);
+      expect(bubbleFinder3, findsNothing);
+
+      final backButtonFinder4 = find.byType(BackButton);
+      expect(backButtonFinder4, findsOneWidget);
+
+      await tester.tap(backButtonFinder4);
+      await tester.pump(const Duration(seconds: 5));
+    });
   });
 }

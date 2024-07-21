14e9918e5d962d2e0b0aae6f2348470457cf3382
Leo Wong <gnowoel@gmail.com>
2024-07-21 21:25:48 +0800

    Fix integration test setup code

--- integration_test/setup.dart
+++ integration_test/setup.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:talktive/services/avatar.dart';
 import 'package:talktive/services/firedata.dart';
 import 'package:talktive/services/history.dart';
 
@@ -17,6 +18,7 @@ Future<void> setup() async {
   await FirebaseAuth.instance.useAuthEmulator(host, 9099);
   FirebaseDatabase.instance.useDatabaseEmulator(host, 9000);
 
-  await History().loadRecords();
+  await Avatar().init();
+  await History().init();
   Firedata().syncTime();
 }

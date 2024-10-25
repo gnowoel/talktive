d856dcc82d51c60dd5cd1b446e3fbb6ffa7e803b
Leo Wong <gnowoel@gmail.com>
2024-10-25 20:29:41 +0800

    Use the fake_firebase_database package from GitHub

--- packages/firebase_database_mocks/lib/firebase_database_mocks.dart
+++
@@ -1,7 +0,0 @@
-// Todo document all files.
-// Todo add license file.
-/// A mock for flutter flutter
-library firebase_database_mocks;
-
-export 'src/mock_firebase_database.dart';
-export 'src/set_up_mocks.dart';

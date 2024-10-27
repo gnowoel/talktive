b186395e4cea4af732420ae27649d1c64f2bc319
Leo Wong <gnowoel@gmail.com>
2024-10-27 14:49:49 +0800

    Make history records dismissible

--- lib/widgets/record_item.dart
+++ lib/widgets/record_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:timeago/timeago.dart';
 
 import '../models/record.dart';
 import '../models/room.dart';
 import '../pages/chat.dart';
+import '../services/history.dart';
 
 class RecordItem extends StatefulWidget {
   final Record record;
@@ -18,6 +20,14 @@ class RecordItem extends StatefulWidget {
 }
 
 class _RecordItemState extends State<RecordItem> {
+  late History history;
+
+  @override
+  void initState() {
+    super.initState();
+    history = Provider.of<History>(context, listen: false);
+  }
+
   void enterChat(Record record) {
     final dummy = Room.fromRecord(record: record);
 
@@ -45,31 +55,39 @@ class _RecordItemState extends State<RecordItem> {
     final textColor =
         byMe ? colorScheme.onTertiaryContainer : colorScheme.onSurface;
 
-    return Card(
-      elevation: 0,
-      margin: const EdgeInsets.symmetric(vertical: 6),
-      color: cardColor,
-      child: GestureDetector(
-        onTap: () => enterChat(widget.record),
-        child: ListTile(
-          leading: Text(
-            widget.record.roomUserCode,
-            style: TextStyle(fontSize: 36, color: textColor),
-          ),
-          title: Text(
-            widget.record.roomTopic,
-            style: TextStyle(color: textColor),
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: Text(
-            format(createdAt),
-            style: TextStyle(color: textColor),
-            overflow: TextOverflow.ellipsis,
-          ),
-          trailing: IconButton(
-            icon: const Icon(Icons.keyboard_arrow_right),
-            onPressed: () => enterChat(widget.record),
-            tooltip: 'Enter room',
+    final roomId = widget.record.roomId;
+
+    return Dismissible(
+      key: Key(roomId),
+      onDismissed: (direction) async {
+        await history.removeRecord(roomId: roomId);
+      },
+      child: Card(
+        elevation: 0,
+        margin: const EdgeInsets.symmetric(vertical: 6),
+        color: cardColor,
+        child: GestureDetector(
+          onTap: () => enterChat(widget.record),
+          child: ListTile(
+            leading: Text(
+              widget.record.roomUserCode,
+              style: TextStyle(fontSize: 36, color: textColor),
+            ),
+            title: Text(
+              widget.record.roomTopic,
+              style: TextStyle(color: textColor),
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Text(
+              format(createdAt),
+              style: TextStyle(color: textColor),
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.keyboard_arrow_right),
+              onPressed: () => enterChat(widget.record),
+              tooltip: 'Enter room',
+            ),
           ),
         ),
       ),

5e9bd3ffa65f8e3341d223a7abe2ba47c9fc0c55
Leo Wong <gnowoel@gmail.com>
2024-11-19 16:12:49 +0800

    Create a Profile page

---
+++ lib/pages/profile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: SafeArea(
+        child: const SizedBox(),
+      ),
+    );
+  }
+}

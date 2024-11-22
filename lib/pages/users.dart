72d4feb2678aa760038a4888fca4da06e2d0cf44
Leo Wong <gnowoel@gmail.com>
2024-11-22 17:34:53 +0800

    Fetch users in `UsersPage`

--- lib/pages/users.dart
+++ lib/pages/users.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/user.dart';
+import '../services/firedata.dart';
 
 class UsersPage extends StatefulWidget {
   const UsersPage({super.key});
@@ -8,6 +12,22 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
+  late Firedata firedata;
+  late List<User> _users;
+
+  @override
+  void initState() {
+    super.initState();
+    firedata = Provider.of<Firedata>(context, listen: false);
+    _users = [];
+    _fetchUsers();
+  }
+
+  Future<void> _fetchUsers() async {
+    final users = await firedata.getUsers();
+    setState(() => _users = [..._users, ...users]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +35,11 @@ class _UsersPageState extends State<UsersPage> {
         title: const Text('Users'),
       ),
       body: SafeArea(
-        child: const SizedBox(),
+        child: ListView.builder(
+            itemCount: _users.length,
+            itemBuilder: (context, index) {
+              return Text(_users[index].displayName!);
+            }),
       ),
     );
   }

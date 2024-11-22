fa6bb4f3aba7ce4356f4d4c6246610895c952841
Leo Wong <gnowoel@gmail.com>
2024-11-22 12:49:06 +0800

    Set user priority based on `languageCode` and `updatedAt`
    https://firebase.google.com/docs/reference/node/firebase.database.Reference

--- functions/src/onUserUpdated.ts
+++ functions/src/onUserUpdated.ts
@@ -1,14 +1,40 @@
 import * as admin from 'firebase-admin';
 import { logger } from 'firebase-functions';
 import { onValueUpdated } from 'firebase-functions/v2/database';
+import { User } from './types';
 
 if (!admin.apps.length) {
   admin.initializeApp();
 }
 
+const db = admin.database();
+
 const onUserUpdated = onValueUpdated('/users/{userId}', async (event) => {
   const userId = event.params.userId;
-  logger.info(userId);
+  const user = event.data.after.val();
+
+  try {
+    await updateUserPriority(userId, user);
+  } catch (error) {
+    logger.error(error);
+  }
 });
+
+const updateUserPriority = async (userId: string, user: User) => {
+  const userRef = db.ref(`users/${userId}`);
+  const languageCode = user.languageCode;
+  const updatedAt = user.updatedAt;
+
+  if (!languageCode || !updatedAt) return;
+
+  const timestamp = new Date(updatedAt).toJSON();
+  const priority = `${languageCode}-${timestamp}`;
+
+  await userRef.setPriority(priority, (error) => {
+    if (error) {
+      logger.error(error);
+    }
+  });
+};
 
 export default onUserUpdated;

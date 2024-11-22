c573d62263c29f0e1faa71d00b363536e54becf8
Leo Wong <gnowoel@gmail.com>
2024-11-22 18:30:42 +0800

    Make the user list look better

--- lib/pages/users.dart
+++ lib/pages/users.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 
 import '../models/user.dart';
 import '../services/firedata.dart';
+import '../widgets/info.dart';
+import '../widgets/user_list.dart';
 
 class UsersPage extends StatefulWidget {
   const UsersPage({super.key});
@@ -30,17 +32,52 @@ class _UsersPageState extends State<UsersPage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    const lines = ['No more users here.', 'Try again later.', ''];
+
     return Scaffold(
+      backgroundColor: theme.colorScheme.surfaceContainerLow,
       appBar: AppBar(
+        backgroundColor: theme.colorScheme.surfaceContainerLow,
         title: const Text('Users'),
       ),
       body: SafeArea(
-        child: ListView.builder(
-            itemCount: _users.length,
-            itemBuilder: (context, index) {
-              return Text(_users[index].displayName!);
-            }),
+        child: _users.isEmpty
+            ? const Center(child: Info(lines: lines))
+            : _buildLayout(_users),
       ),
+    );
+  }
+
+  LayoutBuilder _buildLayout(List<User> users) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final theme = Theme.of(context);
+        if (constraints.maxWidth >= 600) {
+          return Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(24, 12, 24, 24),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.surface,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(24),
+                ),
+                border: Border.all(color: theme.colorScheme.secondaryContainer),
+              ),
+              constraints: const BoxConstraints(minWidth: 324, maxWidth: 576),
+              child: UserList(users: _users),
+            ),
+          );
+        } else {
+          return Container(
+            decoration: BoxDecoration(
+              color: theme.colorScheme.surface,
+            ),
+            child: UserList(users: _users),
+          );
+        }
+      },
     );
   }
 }

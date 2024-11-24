dba560d6b6f36538b758238a5fdd19f291d97320
Leo Wong <gnowoel@gmail.com>
2024-11-24 23:21:36 +0800

    Create a `Chat` model in preparation

---
+++ lib/models/chat.dart
@@ -0,0 +1,15 @@
+class Chat {
+  final String id;
+  final String userId1;
+  final String userId2;
+  final int createdAt;
+  final int updatedAt;
+
+  Chat({
+    required this.id,
+    required this.userId1,
+    required this.userId2,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+}

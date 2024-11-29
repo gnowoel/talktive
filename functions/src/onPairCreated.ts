64fae6e66c98aed63fd180b2e29755c1b885914b
Leo Wong <gnowoel@gmail.com>
2024-11-29 21:55:37 +0800

    Update the `chats` stats when creating a pair

--- functions/src/onPairCreated.ts
+++ functions/src/onPairCreated.ts
@@ -1,7 +1,8 @@
 import * as admin from 'firebase-admin';
 import { logger } from 'firebase-functions';
 import { onValueCreated } from 'firebase-functions/v2/database';
-import { Pair, User } from './types';
+import { Pair, User, StatParams } from './types';
+import { formatDate, isDebugMode } from './helpers';
 
 if (!admin.apps.length) {
   admin.initializeApp();
@@ -13,11 +14,13 @@ const onPairCreated = onValueCreated('/pairs/{pairId}', async (event) => {
   const pairId = event.params.pairId;
   const pair = event.data.val();
   const followers = pair.followers;
+  const now = new Date();
 
   try {
     for (const follower of followers) {
       await copyToFollower(follower, pairId, pair);
     }
+    updateChatStats(now);
   } catch (error) {
     logger.error(error);
   }
@@ -40,6 +43,29 @@ const copyToFollower = async (userId: string, pairId: string, pair: Pair) => {
       createdAt: pair.createdAt,
       updatedAt: pair.updatedAt,
     });
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+const updateChatStats = async (now: Date) => {
+  const statRef = db.ref(`stats/${formatDate(now)}`);
+  const snapshot = await statRef.get();
+
+  if (!snapshot.exists()) return;
+
+  const stat = snapshot.val();
+  const params: StatParams = {};
+
+  // `ServerValue` doesn't work with Emulators Suite
+  if (isDebugMode()) {
+    params.chats = stat.chats + 1;
+  } else {
+    params.chats = admin.database.ServerValue.increment(1);
+  }
+
+  try {
+    await statRef.update(params);
   } catch (error) {
     logger.error(error);
   }

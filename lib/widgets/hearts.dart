19aecb2401cb6d6f153c524bef584ba3e46dbe68
Leo Wong <gnowoel@gmail.com>
2024-12-02 23:59:49 +0800

    Show remaining time when tapping hearts

--- lib/widgets/hearts.dart
+++ lib/widgets/hearts.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 import '../models/chat.dart';
 import '../services/cache.dart';
@@ -58,8 +60,36 @@ class _HeartsState extends State<Hearts> with SingleTickerProviderStateMixin {
     return Duration(milliseconds: now - then);
   }
 
+  String _getRemains(Chat chat) {
+    final now = Cache().now;
+    final then = chat.updatedAt;
+    final elapsed = now - then;
+    final delay = kDebugMode
+        ? 1000 * 60 * 6 // 6 minutes
+        : 1000 * 60 * 60 * 72; // 3 days
+    final diff = delay - elapsed;
+
+    if (chat.isNew) return 'New chat';
+
+    if (diff <= 0) return 'Chat closed';
+
+    var remains = timeago.format(
+      DateTime.fromMillisecondsSinceEpoch(now - diff),
+      locale: 'en_short',
+    );
+
+    if (remains == 'now') {
+      return 'Closing soon';
+    }
+
+    return 'Closing in $remains';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return HeartList(elapsed: elapsed);
+    return Tooltip(
+      message: _getRemains(widget.chat),
+      child: HeartList(elapsed: elapsed),
+    );
   }
 }

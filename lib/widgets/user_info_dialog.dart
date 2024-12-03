e0ef7c0eb238ce0f67350102b489e0a8b22bc8f5
Leo Wong <gnowoel@gmail.com>
2024-12-03 21:28:54 +0800

    Add placeholders for more fields in the dialog

--- lib/widgets/user_info_dialog.dart
+++ lib/widgets/user_info_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'tag.dart';
+
 class UserInfoDialog extends StatelessWidget {
   final String photoURL;
   final String displayName;
@@ -29,6 +31,47 @@ class UserInfoDialog extends StatelessWidget {
               displayName,
               style: theme.textTheme.titleLarge,
               textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              [
+                "This is a line of description.",
+                "In fact, it's a very, very, very long line.",
+                "Can we say, it's a multiline text?",
+              ].join(' '),
+              style: theme.textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 8,
+              runSpacing: 8,
+              children: [
+                Tag(
+                  child: Text(
+                    'F',
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ),
+                Tag(
+                  child: Text(
+                    'en',
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ),
+                Tag(
+                  child: Text(
+                    '2h',
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Close'),
             ),
           ],
         ),

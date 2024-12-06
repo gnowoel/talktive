d66d27f0870d51dc25256f148a44c702e5030ee5
Leo Wong <gnowoel@gmail.com>
2024-12-06 23:00:04 +0800

    Remove some code that is not needed in V2

--- lib/services/firedata.dart
+++ lib/services/firedata.dart
@@ -6,10 +6,8 @@ import 'package:flutter/foundation.dart';
 import '../helpers/exception.dart';
 import '../models/chat.dart';
 import '../models/message.dart';
-import '../models/room.dart';
 import '../models/text_message.dart';
 import '../models/user.dart';
-import 'cache.dart';
 
 class Firedata {
   final FirebaseDatabase instance;
@@ -24,36 +22,6 @@ class Firedata {
     });
 
     return stream;
-  }
-
-  Future<Room> createRoom(
-    String userId,
-    String userName,
-    String userCode,
-    String languageCode,
-  ) async {
-    try {
-      final now = Cache().now;
-      final roomValue = RoomStub(
-        topic: userName, // Use `userName` as the default topic
-        userId: userId,
-        userName: userName,
-        userCode: userCode,
-        languageCode: languageCode,
-        createdAt: now,
-        updatedAt: now,
-        closedAt: now + (kDebugMode ? 360 : 3600) * 1000,
-        filter: '$languageCode-nnnn',
-      );
-
-      final ref = instance.ref('rooms').push();
-
-      await ref.set(roomValue.toJson());
-
-      return Room.fromStub(key: ref.key!, value: roomValue);
-    } catch (e) {
-      throw AppException(e.toString());
-    }
   }
 
   Future<Chat> createPair(String userId, User partner) async {
@@ -143,29 +111,6 @@ class Firedata {
     }
   }
 
-  Future<void> createAccess(String roomId) async {
-    try {
-      final accessRef = instance.ref('accesses').push();
-      final entry = {roomId: true};
-
-      await accessRef.set(entry);
-    } catch (e) {
-      throw AppException(e.toString());
-    }
-  }
-
-  Future<void> updateRoomTopic(Room room, String topic) async {
-    try {
-      if (topic.isEmpty || room.topic == topic) return;
-
-      final ref = instance.ref('rooms/${room.id}');
-
-      await ref.update({'topic': topic});
-    } catch (e) {
-      throw AppException(e.toString());
-    }
-  }
-
   Stream<User?> subscribeToUser(String userId) {
     final ref = instance.ref('users/$userId');
 
@@ -218,29 +163,6 @@ class Firedata {
     } catch (e) {
       throw AppException(e.toString());
     }
-  }
-
-  Stream<Room> subscribeToRoom(String roomId) {
-    final ref = instance.ref('rooms/$roomId');
-
-    final stream = ref.onValue.map((event) {
-      late Room room;
-
-      final snapshot = event.snapshot;
-
-      if (snapshot.exists) {
-        final value = snapshot.value;
-        final json = Map<String, dynamic>.from(value as Map);
-        final stub = RoomStub.fromJson(json);
-        room = Room.fromStub(key: roomId, value: stub);
-      } else {
-        room = Room.dummy();
-      }
-
-      return room;
-    });
-
-    return stream;
   }
 
   Stream<List<Chat>> subscribeToChats(String userId) {
@@ -450,112 +372,6 @@ class Firedata {
       }).toList();
 
       return users;
-    } catch (e) {
-      throw AppException(e.toString());
-    }
-  }
-
-  // Inactive rooms will be removed by scheduled Cloud Functions
-  Future<List<Room>> fetchRooms(
-    String languageCode,
-    List<String> recentRoomIds,
-  ) async {
-    try {
-      final rooms = <Room>[];
-      final expired = <Room>[];
-      var startAt = '$languageCode-0000';
-      var limit = kDebugMode ? 2 : 16;
-      var next = true;
-
-      // TODO: Limit the number of retries if necessary
-      while (next) {
-        final result = await _getRooms(
-          languageCode: languageCode,
-          startAt: startAt,
-          limit: limit,
-        );
-
-        if (result.length < limit) next = false;
-
-        rooms.clear();
-
-        for (final room in result) {
-          if (room.isClosed) {
-            if (!room.isMarkedClosed) {
-              expired.add(room);
-            }
-          } else if (!recentRoomIds.contains(room.id)) {
-            rooms.add(room);
-          }
-        }
-
-        if (rooms.isNotEmpty) next = false;
-
-        if (next) {
-          startAt = result.last.filter;
-          limit *= 2;
-        }
-      }
-
-      rooms.sort((a, b) {
-        int filterComp = a.filter.compareTo(b.filter);
-        if (filterComp == 0) {
-          return a.createdAt.compareTo(b.createdAt);
-        }
-        return filterComp;
-      });
-
-      _markClosed(expired); // No need to wait
-
-      return rooms;
-    } catch (e) {
-      throw AppException(e.toString());
-    }
-  }
-
-  Future<List<Room>> _getRooms({
-    required String languageCode,
-    required String startAt,
-    required int limit,
-  }) async {
-    try {
-      final ref = instance.ref('rooms');
-      final query = ref
-          .orderByChild('filter')
-          .startAt(startAt)
-          .endAt('$languageCode-9999')
-          .limitToFirst(limit);
-      final snapshot = await query.get();
-
-      if (!snapshot.exists) {
-        return [];
-      }
-
-      final map1 = Map<String, dynamic>.from(snapshot.value as Map);
-      final rooms = map1.entries.map((entry) {
-        final map2 = Map<String, dynamic>.from(entry.value as Map);
-        final roomStub = RoomStub.fromJson(map2);
-        return Room.fromStub(key: entry.key, value: roomStub);
-      }).toList();
-
-      rooms.sort((a, b) => a.filter.compareTo(b.filter));
-
-      return rooms;
-    } catch (e) {
-      throw AppException(e.toString());
-    }
-  }
-
-  Future<void> _markClosed(List<Room> rooms) async {
-    try {
-      final expiresRef = instance.ref('expires').push();
-      final collection = <String, dynamic>{};
-
-      for (final room in rooms) {
-        collection[room.id] = true;
-      }
-
-      await expiresRef.set(collection);
     } catch (e) {
       throw AppException(e.toString());
     }

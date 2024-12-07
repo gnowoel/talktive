f466a1c0b9f55d868af71096fe557e15cd3cd70d
Leo Wong <gnowoel@gmail.com>
2024-12-08 00:10:33 +0800

    Swipe to dismiss chat entries

--- lib/widgets/chat_item.dart
+++ lib/widgets/chat_item.dart
@@ -7,6 +7,7 @@ import '../models/chat.dart';
 import '../pages/chat.dart';
 import '../services/cache.dart';
 import '../services/fireauth.dart';
+import '../services/firedata.dart';
 import 'tag.dart';
 
 class ChatItem extends StatefulWidget {
@@ -23,22 +24,46 @@ class ChatItem extends StatefulWidget {
 
 class _ChatItemState extends State<ChatItem> {
   late Fireauth fireauth;
+  late Firedata firedata;
   late bool byMe;
 
   @override
   void initState() {
     super.initState();
     fireauth = context.read<Fireauth>();
+    firedata = context.read<Firedata>();
     byMe = widget.chat.firstUserId == fireauth.instance.currentUser!.uid;
   }
 
-  void enterChat() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => ChatPage(chat: widget.chat),
-      ),
-    );
+  Future<void> _muteChat() async {
+    _doAction(() async {
+      await firedata.updateChat(
+        fireauth.instance.currentUser!.uid,
+        widget.chat.id,
+        mute: true,
+      );
+    });
+  }
+
+  Future<void> _enterChat() async {
+    _doAction(() async {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatPage(chat: widget.chat),
+        ),
+      );
+    });
+  }
+
+  Future<void> _doAction(Future<void> Function() action) async {
+    try {
+      await action();
+    } on AppException catch (e) {
+      if (mounted) {
+        ErrorHandler.showSnackBarMessage(context, e);
+      }
+    }
   }
 
   @override
@@ -58,82 +83,89 @@ class _ChatItemState extends State<ChatItem> {
         (widget.chat.lastMessageContent ?? widget.chat.partner.description!)
             .replaceAll(RegExp(r'\s+'), ' ');
 
-    return Card(
-      elevation: 0,
-      margin: const EdgeInsets.symmetric(vertical: 6),
-      color: cardColor,
-      child: GestureDetector(
-        onTap: enterChat,
-        child: ListTile(
-          leading: Text(
-            widget.chat.partner.photoURL!,
-            style: TextStyle(fontSize: 36, color: textColor),
-          ),
-          title: Text(
-            widget.chat.partner.displayName!,
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                lastMessageContent,
-                overflow: TextOverflow.ellipsis,
-              ),
-              Row(
-                children: [
-                  Tag(
-                    tooltip:
-                        '${getLongGenderName(widget.chat.partner.gender!)}',
-                    child: Text(
-                      widget.chat.partner.gender!,
-                      style: TextStyle(fontSize: 12),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  const SizedBox(width: 4),
-                  Tag(
-                    tooltip:
-                        '${getLanguageName(widget.chat.partner.languageCode!)}',
-                    child: Text(
-                      widget.chat.partner.languageCode!,
-                      style: TextStyle(fontSize: 12),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  const SizedBox(width: 4),
-                  Tag(
-                    tooltip: 'Last updated',
-                    child: Text(
-                      timeago.format(updatedAt, locale: 'en_short', clock: now),
-                      style: TextStyle(fontSize: 12),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          trailing: newMessageCount > 0
-              ? Badge(
-                  label: Text(
-                    '$newMessageCount',
-                    style: TextStyle(
-                      fontSize: 14,
-                    ),
-                  ),
-                  backgroundColor: colorScheme.error,
-                )
-              : Badge(
-                  label: Text(
-                    '$newMessageCount',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: colorScheme.surfaceContainerLow,
-                    ),
-                  ),
-                  backgroundColor: colorScheme.outline,
+    return Dismissible(
+      key: Key(widget.chat.id),
+      onDismissed: (direction) async {
+        await _muteChat();
+      },
+      child: Card(
+        elevation: 0,
+        margin: const EdgeInsets.symmetric(vertical: 6),
+        color: cardColor,
+        child: GestureDetector(
+          onTap: _enterChat,
+          child: ListTile(
+            leading: Text(
+              widget.chat.partner.photoURL!,
+              style: TextStyle(fontSize: 36, color: textColor),
+            ),
+            title: Text(
+              widget.chat.partner.displayName!,
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  lastMessageContent,
+                  overflow: TextOverflow.ellipsis,
                 ),
+                Row(
+                  children: [
+                    Tag(
+                      tooltip:
+                          '${getLongGenderName(widget.chat.partner.gender!)}',
+                      child: Text(
+                        widget.chat.partner.gender!,
+                        style: TextStyle(fontSize: 12),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    Tag(
+                      tooltip:
+                          '${getLanguageName(widget.chat.partner.languageCode!)}',
+                      child: Text(
+                        widget.chat.partner.languageCode!,
+                        style: TextStyle(fontSize: 12),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    Tag(
+                      tooltip: 'Last updated',
+                      child: Text(
+                        timeago.format(updatedAt,
+                            locale: 'en_short', clock: now),
+                        style: TextStyle(fontSize: 12),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            trailing: newMessageCount > 0
+                ? Badge(
+                    label: Text(
+                      '$newMessageCount',
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                    backgroundColor: colorScheme.error,
+                  )
+                : Badge(
+                    label: Text(
+                      '$newMessageCount',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: colorScheme.surfaceContainerLow,
+                      ),
+                    ),
+                    backgroundColor: colorScheme.outline,
+                  ),
+          ),
         ),
       ),
     );

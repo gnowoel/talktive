3d9f1697f38d1b4fc3288c1db537e2b514c90a8c
Leo Wong <gnowoel@gmail.com>
2024-12-10 22:35:53 +0800

    Add push notification listeners

--- lib/notifier.dart
+++ lib/notifier.dart
@@ -28,53 +28,63 @@ class _NotifierState extends State<Notifier> {
   @override
   void initState() {
     super.initState();
-
     firedata = context.read<Firedata>();
     messaging = context.read<Messaging>();
     cache = context.read<Cache>();
-
     _user = cache.user;
-    _initNotification();
+    _firstTimeSetup();
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-
     cache = Provider.of<Cache>(context);
-
     _user = cache.user;
-    _changeNotification();
+    _storeFcmToken();
   }
 
-  Future<void> _initNotification() async {
+  Future<void> _firstTimeSetup() async {
+    try {
+      await _requestPermission();
+      await _storeFcmToken();
+      await _addListeners();
+    } catch (e) {
+      // Ignore on unsupported platforms
+    }
+  }
+
+  Future<void> _requestPermission() async {
     try {
       await messaging.instance.requestPermission(
         alert: true,
         badge: true,
         sound: true,
       );
-
-      await _changeNotification();
     } catch (e) {
       // Ignore on unsupported platforms
     }
   }
 
-  Future<void> _changeNotification() async {
+  Future<void> _storeFcmToken() async {
     try {
       if (_user?.fcmToken == null) {
         _fcmToken = _fcmToken ?? await messaging.instance.getToken();
         await firedata.setUserFcmToken(_user?.id, _fcmToken);
       }
-
       fcmTokenSubscription?.cancel();
-
       fcmTokenSubscription =
           messaging.subscribeToFcmToken().listen((token) async {
         _fcmToken = token;
         await firedata.setUserFcmToken(_user?.id, _fcmToken);
       });
+    } catch (e) {
+      // Ignore on unsupported platforms
+    }
+  }
+
+  Future<void> _addListeners() async {
+    try {
+      await messaging.addListeners();
     } catch (e) {
       // Ignore on unsupported platforms
     }

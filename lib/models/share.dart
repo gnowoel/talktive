5d4f42f2e7be9fe860087143abbaf2e73207d179
Leo Wong <gnowoel@gmail.com>
2024-12-25 21:59:11 +0800

    Add a Share model for group chats

---
+++ lib/models/share.dart
@@ -0,0 +1,21 @@
+class Share {
+  final String id;
+  final String topic;
+  final String userId;
+  final String userDisplayName;
+  final String userPhotoURL;
+  final String languageCode;
+  final int createdAt;
+  final int updatedAt;
+
+  Share({
+    required this.id,
+    required this.topic,
+    required this.userId,
+    required this.userDisplayName,
+    required this.userPhotoURL,
+    required this.languageCode,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+}

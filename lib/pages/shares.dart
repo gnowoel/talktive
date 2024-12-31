4a765b76353e27616473351e57533e94940396e3
Leo Wong <gnowoel@gmail.com>
2024-12-31 16:55:30 +0800

    Add a placeholder Shares page

---
+++ lib/pages/shares.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SharesPage extends StatelessWidget {
+  const SharesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Shares'),
+    );
+  }
+}

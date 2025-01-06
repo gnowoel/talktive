74552bb9ddd110cad0b69623fdcf60626f76e63b
Leo Wong <gnowoel@gmail.com>
2025-01-06 22:09:44 +0800

    Add more utility methods to `Settings`

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -3,6 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart' as prefs;
 class Settings {
   static const _notificationKey = 'notification_permission_requested';
 
+  static const hasCompletedSetup = 'hasCompletedSetup';
+
   static Future<bool> hasRequestedNotificationPermission() async {
     final preferences = await prefs.SharedPreferences.getInstance();
     return preferences.getBool(_notificationKey) ?? false;
@@ -11,5 +13,25 @@ class Settings {
   static Future<void> markNotificationPermissionRequested() async {
     final preferences = await prefs.SharedPreferences.getInstance();
     await preferences.setBool(_notificationKey, true);
+  }
+
+  static Future<String?> getString(String key) async {
+    final preferences = await prefs.SharedPreferences.getInstance();
+    return preferences.getString(key);
+  }
+
+  static Future<void> setString(String key, String value) async {
+    final preferences = await prefs.SharedPreferences.getInstance();
+    await preferences.setString(key, value);
+  }
+
+  static Future<bool> getBool(String key) async {
+    final preferences = await prefs.SharedPreferences.getInstance();
+    return preferences.getBool(key) ?? false;
+  }
+
+  static Future<void> setBool(String key, bool value) async {
+    final preferences = await prefs.SharedPreferences.getInstance();
+    await preferences.setBool(key, value);
   }
 }

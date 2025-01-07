30baf92c814608b153797d544cd9f1ec8e9cb540
Leo Wong <gnowoel@gmail.com>
2025-01-07 13:11:32 +0800

    Wrap `MaterialApp` with `Providers`

--- lib/app.dart
+++ lib/app.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'router.dart';
 import 'theme.dart';
+import 'wrappers/providers.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-      title: 'Talktive',
-      theme: getTheme(context),
+    return Providers(
+      child: MaterialApp.router(
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+        title: 'Talktive',
+        theme: getTheme(context),
+      ),
     );
   }
 }

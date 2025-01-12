82d1f9ac2bcf667984e978b329c7e3602fb901a3
Leo Wong <gnowoel@gmail.com>
2025-01-12 22:13:55 +0800

    Periodically check the total unread message count

--- lib/widgets/navigation.dart
+++ lib/widgets/navigation.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../helpers/messages.dart';
+import '../helpers/time.dart';
+import '../models/chat.dart';
 import '../services/cache.dart';
 
-class Navigation extends StatelessWidget {
+class Navigation extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
 
   const Navigation({
@@ -13,20 +17,59 @@ class Navigation extends StatelessWidget {
     required this.navigationShell,
   });
 
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  late Cache cache;
+  List<Chat> _chats = [];
+  Timer? _timer;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    cache = Provider.of<Cache>(context);
+    _setChatsAgain();
+  }
+
+  void _setChatsAgain() {
+    _chats = cache.chats.where((chat) => chat.isActive).toList();
+
+    final nextTime = getNextTime(_chats);
+
+    if (nextTime == null) return;
+
+    final duration = Duration(milliseconds: nextTime);
+
+    _timer?.cancel();
+
+    _timer = Timer(duration, () {
+      setState(() {
+        _setChatsAgain();
+      });
+    });
+  }
+
   void _goBranch(int index) {
-    navigationShell.goBranch(index);
+    widget.navigationShell.goBranch(index);
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final currentIndex = navigationShell.currentIndex;
-    final chats = context.select((Cache cache) => cache.chats);
-    final unreadCount = chatsUnreadMessageCount(chats);
+    final currentIndex = widget.navigationShell.currentIndex;
+    final unreadCount = chatsUnreadMessageCount(_chats);
 
     return Scaffold(
-      body: navigationShell,
+      body: widget.navigationShell,
       bottomNavigationBar: NavigationBar(
-        selectedIndex: navigationShell.currentIndex,
+        selectedIndex: widget.navigationShell.currentIndex,
         destinations: [
           NavigationDestination(
             label: 'Users',

5a178f35a936c78a698c8f2020c594b9d0c4496a
Leo Wong <gnowoel@gmail.com>
2025-01-24 16:34:34 +0800

    Add the `Report.partnerDisplayName` field

--- lib/models/report.dart
+++ lib/models/report.dart
@@ -2,7 +2,7 @@ class Report {
   final String id;
   final String userId;
   final String chatId;
-  final String partnerId;
+  final String? partnerDisplayName;
   final String status;
   final String? resolution;
   final String? adminId;
@@ -13,7 +13,7 @@ class Report {
     required this.id,
     required this.userId,
     required this.chatId,
-    required this.partnerId,
+    this.partnerDisplayName,
     required this.status,
     this.resolution,
     this.adminId,
@@ -26,7 +26,7 @@ class Report {
       id: id,
       userId: json['userId'] as String,
       chatId: json['chatId'] as String,
-      partnerId: json['partnerId'] as String,
+      partnerDisplayName: json['partnerDisplayName'] as String?,
       status: json['status'] as String,
       resolution: json['resolution'] as String?,
       adminId: json['adminId'] as String?,

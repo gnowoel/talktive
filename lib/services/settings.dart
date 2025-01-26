749a8d47a800f84b93e408e9d3616568950840de
Leo Wong <gnowoel@gmail.com>
2025-01-26 22:13:59 +0800

    Ask users to walk through the new setup wizard

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -1,25 +1,25 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+final wizardVersion = '3.0.4+28';
+
 class Settings {
   Settings._();
-
   static final Settings _instance = Settings._();
-
   factory Settings() => _instance;
 
-  bool _hasCompletedSetup = false;
-
-  bool get hasCompletedSetup => _hasCompletedSetup;
-
-  Future<void> markSetupComplete() async {
-    await Prefs.setBool('hasCompletedSetup', true);
-    _hasCompletedSetup = true;
-  }
+  String? _completedSetupVersion;
 
   Future<void> load() async {
-    final value = await Prefs.getBool('hasCompletedSetup');
-    _hasCompletedSetup = value;
+    final value = await Prefs.getString('completedSetupVersion');
+    _completedSetupVersion = value;
   }
+
+  Future<void> markSetupComplete() async {
+    await Prefs.setString('completedSetupVersion', wizardVersion);
+    _completedSetupVersion = wizardVersion;
+  }
+
+  bool get hasCompletedSetup => _completedSetupVersion == wizardVersion;
 }
 
 class Prefs {
@@ -31,5 +31,15 @@ class Prefs {
   static Future<void> setBool(String key, bool value) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(key, value);
+  }
+
+  static Future<String?> getString(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  static Future<void> setString(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
   }
 }

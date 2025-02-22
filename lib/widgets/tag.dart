31341d305ca377c02702bf04e2b0fb138aad6802
Leo Wong <gnowoel@gmail.com>
2025-02-22 17:23:08 +0800

    Simply the usage of the `Tag` widget

--- lib/widgets/tag.dart
+++ lib/widgets/tag.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 class Tag extends StatelessWidget {
   final String? status;
-  final String tooltip;
-  final Widget child;
+  final String? tooltip;
+  final Widget? child;
 
   const Tag({
     super.key,
     this.status,
-    required this.tooltip,
-    required this.child,
+    this.tooltip,
+    this.child,
   });
 
   @override
@@ -18,59 +18,81 @@ class Tag extends StatelessWidget {
 
     if (status == 'warning') {
       return Tooltip(
-        message: tooltip,
+        message: 'Reported for inappropriate behavior',
         child: Container(
           margin: const EdgeInsets.only(top: 6, bottom: 2),
           padding: const EdgeInsets.symmetric(horizontal: 8),
           decoration: BoxDecoration(
             color: theme.colorScheme.errorContainer,
-            // border: Border.all(color: theme.colorScheme.error),
             border: Border.all(color: theme.colorScheme.inversePrimary),
             borderRadius: BorderRadius.all(
               Radius.circular(16),
             ),
           ),
-          child: child,
+          child: Text(
+            'warning',
+            style: TextStyle(
+              fontSize: 12,
+              color: theme.colorScheme.onErrorContainer,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       );
     }
 
     if (status == 'alert') {
       return Tooltip(
-        message: tooltip,
+        message: 'Reported for offensive messages',
         child: Container(
           margin: const EdgeInsets.only(top: 6, bottom: 2),
           padding: const EdgeInsets.symmetric(horizontal: 8),
           decoration: BoxDecoration(
             color: theme.colorScheme.tertiaryContainer,
-            // border: Border.all(color: theme.colorScheme.error),
             border: Border.all(color: theme.colorScheme.inversePrimary),
             borderRadius: BorderRadius.all(
               Radius.circular(16),
             ),
           ),
-          child: child,
+          child: Text(
+            'alert',
+            style: TextStyle(
+              fontSize: 12,
+              color: theme.colorScheme.onTertiaryContainer,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       );
     }
 
     if (status == 'newcomer') {
       return Tooltip(
-        message: tooltip,
+        message: 'Created in less than 1 day',
         child: Container(
           margin: const EdgeInsets.only(top: 6, bottom: 2),
           padding: const EdgeInsets.symmetric(horizontal: 8),
           decoration: BoxDecoration(
-            color: theme.colorScheme.secondaryContainer,
-            // border: Border.all(color: theme.colorScheme.error),
+            color: theme.colorScheme.primaryContainer,
             border: Border.all(color: theme.colorScheme.inversePrimary),
             borderRadius: BorderRadius.all(
               Radius.circular(16),
             ),
           ),
-          child: child,
+          child: Text(
+            'new',
+            style: TextStyle(
+              fontSize: 12,
+              color: theme.colorScheme.onPrimaryContainer,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       );
+    }
+
+    if (tooltip == null || child == null) {
+      return const SizedBox.shrink();
     }
 
     return Tooltip(

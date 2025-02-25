602c11dd632f012e7f4a697726eb8a5c89d9ff47
Leo Wong <gnowoel@gmail.com>
2025-02-25 21:44:49 +0800

    Remove the `getLongGenderDescription()` method

--- lib/helpers/gender.dart
+++ lib/helpers/gender.dart
@@ -5,18 +5,6 @@ const Map<String, String> genderNames = {
   'X': 'Unknown',
 };
 
-// TODO: Remove this
-const Map<String, String> genderDescriptions = {
-  'F': 'Female',
-  'M': 'Male',
-  'O': 'Other', // 'Other gender'
-  'X': 'Unknown', // 'Gender unknown'
-};
-
 String? getLongGenderName(String shortName) {
   return genderNames[shortName];
-}
-
-String? getLongGenderDescription(String shortName) {
-  return genderDescriptions[shortName];
 }

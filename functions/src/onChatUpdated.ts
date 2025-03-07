5c2a61e84e962fc884559cb1a771a2aea4de244b
Leo Wong <gnowoel@gmail.com>
2025-03-07 09:23:02 +0800

    Update chat priority only when necessary

--- functions/src/onChatUpdated.ts
+++ functions/src/onChatUpdated.ts
@@ -12,16 +12,20 @@ const db = admin.database();
 const onChatUpdated = onValueUpdated('/chats/{userId}/{chatId}', async (event) => {
   const userId = event.params.userId;
   const chatId = event.params.chatId;
+  const chatBefore = event.data.before.val();
   const chat = event.data.after.val();
 
   try {
-    await updateChatPriority(userId, chatId, chat);
+    await updateChatPriority(userId, chatId, chat, chatBefore);
   } catch (error) {
     logger.error(error);
   }
 });
 
-const updateChatPriority = async (userId: string, chatId: string, chat: Chat) => {
+// TODO: We've never used chat priority.
+const updateChatPriority = async (userId: string, chatId: string, chat: Chat, chatBefore: Chat) => {
+  if (chat.updatedAt === chatBefore.updatedAt) return;
+
   const chatRef = db.ref(`chats/${userId}/${chatId}`);
   const priority = -1 * chat.updatedAt;
 

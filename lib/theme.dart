6e18b4e9b4ee64427c6944516b9dd7ee944921c6
Leo Wong <gnowoel@gmail.com>
2025-03-10 17:47:20 +0800

    Define custom theme colors for friend indicator

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
 
+class CustomColors extends ThemeExtension<CustomColors> {
+  final Color friendIndicator;
+
+  const CustomColors({required this.friendIndicator});
+
+  @override
+  ThemeExtension<CustomColors> copyWith({Color? friendIndicator}) {
+    return CustomColors(
+      friendIndicator: friendIndicator ?? this.friendIndicator,
+    );
+  }
+
+  @override
+  ThemeExtension<CustomColors> lerp(
+    ThemeExtension<CustomColors>? other,
+    double t,
+  ) {
+    if (other is! CustomColors) {
+      return this;
+    }
+    return CustomColors(
+      friendIndicator: Color.lerp(friendIndicator, other.friendIndicator, t)!,
+    );
+  }
+
+  static const light = CustomColors(
+    friendIndicator: Color(0xFFFFB74D), // Orange 300
+  );
+
+  static const dark = CustomColors(
+    friendIndicator: Color(0xFFFFD180), // Orange A100
+  );
+}
+
 ThemeData getTheme(BuildContext context) {
+  final brightness = MediaQuery.platformBrightnessOf(context);
+  final isDark = brightness == Brightness.dark;
+
   return ThemeData(
     useMaterial3: true,
     colorScheme: ColorScheme.fromSeed(
-      brightness: MediaQuery.platformBrightnessOf(context),
+      brightness: brightness,
       seedColor: Colors.lightGreen,
     ),
+    extensions: [isDark ? CustomColors.dark : CustomColors.light],
   );
 }

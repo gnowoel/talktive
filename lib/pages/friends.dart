27a5b24f490a0935bfb9994a719bccfeee8e4a5f
Leo Wong <gnowoel@gmail.com>
2025-03-11 19:01:37 +0800

    Update the `Friends` page

--- lib/pages/friends.dart
+++ lib/pages/friends.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class FriendsPage extends StatelessWidget {
+import '../models/friend.dart';
+import '../services/friend_cache.dart';
+import '../widgets/friend_list.dart';
+import '../widgets/info.dart';
+import '../widgets/layout.dart';
+
+class FriendsPage extends StatefulWidget {
   const FriendsPage({super.key});
 
   @override
+  State<FriendsPage> createState() => _FriendsPageState();
+}
+
+class _FriendsPageState extends State<FriendsPage> {
+  late FriendCache friendCache;
+
+  List<Friend> _friends = [];
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    friendCache = Provider.of<FriendCache>(context);
+    _friends = friendCache.friends;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final lines = ['No friends yet. Add some', 'friends to chat with!', ''];
+
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(child: Center(child: const Text('Friends'))),
+      backgroundColor: theme.colorScheme.surfaceContainerLow,
+      appBar: AppBar(
+        backgroundColor: theme.colorScheme.surfaceContainerLow,
+        title: const Text('My Friends'),
+      ),
+      body: SafeArea(
+        child:
+            _friends.isEmpty
+                ? Center(child: Info(lines: lines))
+                : Layout(child: FriendList(friends: _friends)),
+      ),
     );
   }
 }

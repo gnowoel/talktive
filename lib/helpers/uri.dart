5b116e0b515a7d26069716fa6b99dd4ade6365e3
Leo Wong <gnowoel@gmail.com>
2025-03-28 23:22:31 +0800

    Add network image caching with loading states

--- lib/helpers/uri.dart
+++ lib/helpers/uri.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 const _a = 'http://10.0.2.2';
 const _b = 'http://localhost';
@@ -16,4 +18,26 @@ bool get _isAndroid {
   if (kIsWeb) return false;
   if (Platform.isAndroid) return true;
   return false;
+}
+
+CachedNetworkImageProvider getCachedImageProvider(String uri) {
+  return CachedNetworkImageProvider(
+    convertUri(uri),
+    // TODO: Configure caching parameters
+    cacheKey: uri, // Using the original uri as cache key
+  );
+}
+
+Widget getImagePlaceholder({Color? color}) {
+  return Center(
+    child: SizedBox(
+      width: 24,
+      height: 24,
+      child: CircularProgressIndicator(strokeWidth: 2, color: color),
+    ),
+  );
+}
+
+Widget getImageErrorWidget() {
+  return Icon(Icons.broken_image_rounded, size: 40, color: Colors.grey[400]);
 }

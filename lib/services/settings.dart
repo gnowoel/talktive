a29225f724a9e50c3db96c90e74f8081fc7fd911
Leo Wong <gnowoel@gmail.com>
2025-04-08 22:49:34 +0800

    Add a `WhatsNew` page

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -1,5 +1,6 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+final whatsNewVersion = '3.9.0+53';
 final wizardVersion = '3.0.4+28';
 
 class Settings {
@@ -7,6 +8,7 @@ class Settings {
   static final Settings _instance = Settings._();
   factory Settings() => _instance;
 
+  String? _seenWhatsNewVersion;
   String? _completedSetupVersion;
   bool _hasHiddenUsersNotice = false;
   bool _hasHiddenChatsNotice = false;
@@ -14,6 +16,8 @@ class Settings {
   String? _selectedLanguage;
 
   Future<void> load() async {
+    _seenWhatsNewVersion = await Prefs.getString('seenWhatsNewVersion');
+
     final setupVersion = await Prefs.getString('completedSetupVersion');
     final usersNotice = await Prefs.getBool('hasHiddenUsersNotice');
     final chatsNotice = await Prefs.getBool('hasHiddenChatsNotice');
@@ -25,6 +29,11 @@ class Settings {
     _hasHiddenChatsNotice = chatsNotice;
     _selectedGender = selectedGender;
     _selectedLanguage = selectedLanguage;
+  }
+
+  Future<void> setSeenWhatsNewVersion() async {
+    await Prefs.setString('seenWhatsNewVersion', whatsNewVersion);
+    _seenWhatsNewVersion = whatsNewVersion;
   }
 
   Future<void> markSetupComplete() async {
@@ -74,6 +83,7 @@ class Settings {
     _selectedLanguage = null;
   }
 
+  bool get hasSeenWhatsNew => _seenWhatsNewVersion == whatsNewVersion;
   bool get hasCompletedSetup => _completedSetupVersion == wizardVersion;
   bool get hasHiddenUsersNotice => _hasHiddenUsersNotice;
   bool get hasHiddenChatsNotice => _hasHiddenChatsNotice;

579ef0309984307368a4b4eaf349a3da5a9f0f5c
Leo Wong <gnowoel@gmail.com>
2025-04-11 12:07:02 +0800

    Clear notifications when app enters foreground

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'router.dart';
+import 'services/messaging.dart';
 import 'theme.dart';
 import 'wrappers/verify_user.dart';
 import 'wrappers/current_user.dart';
@@ -13,8 +14,33 @@ import 'wrappers/whats_new.dart';
 
 const useEmulators = true;
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      final messaging = Messaging();
+      messaging.clearAllNotifications();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {

30e5922070157a7e37f2297ab31e3a425c5df076
Leo Wong <gnowoel@gmail.com>
2025-04-13 23:42:09 +0800

    Move FCM token storage to CurrentUser wrapper
    This ensures that the FCM token is initialized even if it is accidentally removed and the setup wizard is skipped.

--- lib/wrappers/current_user.dart
+++ lib/wrappers/current_user.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/user.dart';
+import '../services/firedata.dart';
 import '../services/user_cache.dart';
 import '../theme.dart';
 
@@ -14,17 +16,33 @@ class CurrentUser extends StatefulWidget {
 }
 
 class _CurrentUserState extends State<CurrentUser> {
+  late Firedata firedata;
   late UserCache userCache;
+
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    firedata = context.read<Firedata>();
+  }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     userCache = Provider.of<UserCache>(context);
+    _user = userCache.user;
+
+    if (_user != null) {
+      if (_user!.fcmToken == null) {
+        firedata.storeFcmToken(_user!.id); // No wait
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (userCache.user == null) {
+    if (_user == null) {
       return MaterialApp(
         theme: getTheme(context),
         home: const Scaffold(

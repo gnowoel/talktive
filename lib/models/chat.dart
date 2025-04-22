f45ce9d3ba5e234af1e48f26d1b7ebb3c65e4d98
Leo Wong <gnowoel@gmail.com>
2025-04-22 11:32:51 +0800

    Add more fields to the `PublicTopic` model

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -1,5 +1,17 @@
 abstract class Chat {
+  final String id;
+  final int createdAt;
+  final int updatedAt;
+  final int messageCount;
   final String type; // 'chat' or 'topic'
+  final int? readMessageCount;
 
-  const Chat({required this.type});
+  const Chat({
+    required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.messageCount,
+    required this.type,
+    this.readMessageCount,
+  });
 }
--- lib/models/public_topic.dart
+++ lib/models/public_topic.dart
@@ -1,21 +1,24 @@
+import '../helpers/time.dart';
+import '../services/server_clock.dart';
 import 'chat.dart';
 import 'user.dart';
 
 class PublicTopic extends Chat {
-  final String id;
   final String title;
-  final int createdAt;
-  final int updatedAt;
-  final UserStub creator;
-  final int messageCount;
+  final User creator;
+  final String? lastMessageContent;
+  final bool? mute;
 
   const PublicTopic({
-    required this.id,
+    required super.id,
+    required super.createdAt,
+    required super.updatedAt,
+    required super.messageCount,
     required this.title,
-    required this.createdAt,
-    required this.updatedAt,
     required this.creator,
-    required this.messageCount,
+    super.readMessageCount,
+    this.lastMessageContent,
+    this.mute,
   }) : super(type: 'topic');
 
   Map<String, dynamic> toJson() {
@@ -26,19 +29,37 @@ class PublicTopic extends Chat {
       'updatedAt': updatedAt,
       'creator': creator.toJson(),
       'messageCount': messageCount,
+      'readMessageCount': readMessageCount,
+      'lastMessageContent': lastMessageContent,
+      'mute': mute,
     };
   }
 
   factory PublicTopic.fromJson(String id, Map<String, dynamic> json) {
+    final creatorMap = Map<String, dynamic>.from(json['creator'] as Map);
+    final creator = User.fromStub(
+      key: creatorMap['id'] as String,
+      value: UserStub.fromJson(creatorMap),
+    );
+
     return PublicTopic(
       id: id,
       title: json['title'] as String,
       createdAt: json['createdAt'] as int,
       updatedAt: json['updatedAt'] as int,
-      creator: UserStub.fromJson(
-        Map<String, dynamic>.from(json['creator'] as Map),
-      ),
+      creator: creator,
       messageCount: json['messageCount'] as int,
+      readMessageCount: json['readMessageCount'] as int?,
+      lastMessageContent: json['lastMessageContent'] as String?,
+      mute: json['mute'] as bool?,
     );
   }
+
+  bool get isClosed => updatedAt + delay <= ServerClock().now;
+  bool get isNotClosed => !isClosed;
+
+  bool get isMuted => mute ?? false;
+  bool get isNotMuted => !isMuted;
+
+  bool get isActive => isNotClosed && isNotMuted;
 }

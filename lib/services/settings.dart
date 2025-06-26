5f93a899a3aacfc86d81ad5797d4555e206a878f
Leo Wong <gnowoel@gmail.com>
2025-06-27 00:34:18 +0800

    Communicate rules with helpful infos

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 final whatsNewVersion = '4.2.0+57';
 final setupWizardVersion = '3.0.4+28';
 final usersPageNoticeVersion = 'true'; // Next time will use verion number
-final chatsPageNoticeVersion = 'true'; // Next time will use verion number
+final chatsPageNoticeVersion = '4.2.1+58';
 
 class Settings {
   Settings._();

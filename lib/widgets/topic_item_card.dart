4f04e0d44e8749e202e9b5a4a77ac31dfe759a4b
Leo Wong <gnowoel@gmail.com>
2025-08-07 00:05:19 +0800

    Make the intro tags stand out

--- lib/widgets/topic_item_card.dart
+++ lib/widgets/topic_item_card.dart
@@ -234,17 +234,7 @@ class _TopicItemCardState extends State<TopicItemCard> {
                     ),
                     if (widget.topic.creator.followerCount == 0) ...[
                       const SizedBox(width: 4),
-                      Tag(
-                        tooltip: 'New member introduction',
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.waving_hand, size: 12),
-                            const SizedBox(width: 4),
-                            Text('Intro', style: TextStyle(fontSize: 12)),
-                          ],
-                        ),
-                      ),
+                      Tag(status: 'introduction'),
                     ],
                   ],
                 ),

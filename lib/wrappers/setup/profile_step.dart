fa445f9933ef358924612596a41ac976b5b70a33
Leo Wong <gnowoel@gmail.com>
2025-08-07 18:18:12 +0800

    Update form field hints

--- lib/wrappers/setup/profile_step.dart
+++ lib/wrappers/setup/profile_step.dart
@@ -232,7 +232,7 @@ class _ProfileStepState extends State<ProfileStep> {
                         controller: _descriptionController,
                         decoration: const InputDecoration(
                           labelText: 'Current Status',
-                          hintText: 'What would you like to share right now?',
+                          hintText: 'Tell us who you are and why you\'re here.',
                         ),
                         validator: _validateDescription,
                         minLines: 2,

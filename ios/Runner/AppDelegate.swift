a2a2e6b3c2f7d7f09db8b4774486a4c01fa9aead
Leo Wong <gnowoel@gmail.com>
2025-12-20 00:57:10 +0800

    fix(ios): finish UIScene launch wiring
    - Add UIScene storyboard config so Main.storyboard loads
    
    - Include SceneDelegate.swift in Runner target sources
    
    - Let Info.plist drive scene configuration (no AppDelegate override)
    
    - Remove temporary native debug logging

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,21 +7,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    print("NATIVE_DEBUG: AppDelegate didFinishLaunchingWithOptions started")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    print("NATIVE_DEBUG: didInitializeImplicitFlutterEngine called")
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-    print("NATIVE_DEBUG: GeneratedPluginRegistrant finished")
-  }
-
-  override func application(
-    _ application: UIApplication,
-    configurationForConnecting connectingSceneSession: UISceneSession,
-    options: UIScene.ConnectionOptions
-  ) -> UISceneConfiguration {
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
 }

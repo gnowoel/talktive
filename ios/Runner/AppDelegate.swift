00a785ca83077bcf31b8a7027c393c94c3e24f6a
Leo Wong <gnowoel@gmail.com>
2025-12-19 21:50:12 +0800

    fix(ios): correct UIScene migration to use FlutterSceneDelegate
    - Adopt FlutterImplicitEngineDelegate in AppDelegate
    - Move plugin registration to didInitializeImplicitFlutterEngine
    - Change SceneDelegate to subclass FlutterSceneDelegate instead of UIWindowSceneDelegate
    - Remove manual FlutterEngine access that caused 'FlutterEngine was already invoked' error
    - Add AdMob privacy settings (NSUserTrackingUsageDescription, SKAdNetworkItems)
    
    This fixes the black screen issue and prevents the engine from being invoked multiple times.
    The 'Communicating on a dead channel' errors still need investigation.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,13 +2,19 @@ import UIKit
 import Flutter
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    print("NATIVE_DEBUG: AppDelegate didFinishLaunchingWithOptions started")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    print("NATIVE_DEBUG: didInitializeImplicitFlutterEngine called")
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    print("NATIVE_DEBUG: GeneratedPluginRegistrant finished")
   }
 
   override func application(

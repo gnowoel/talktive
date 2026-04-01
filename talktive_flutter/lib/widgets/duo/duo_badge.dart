dfaaf9b1d162322fa0332795d4dedd284ccac9a8
Leo Wong <gnowoel@gmail.com>
2026-04-01 22:08:06 +0800

    ui: Refined DuoBadge UI with a modern professional notification design

--- talktive_flutter/lib/widgets/duo/duo_badge.dart
+++ talktive_flutter/lib/widgets/duo/duo_badge.dart
@@ -30,31 +30,33 @@ class DuoBadge extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          // Badge circle
-          Container(
-                width: 72,
-                height: 72,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: isUnlocked ? Colors.white : Colors.grey[100],
-                  border: Border.all(
-                    color: isUnlocked ? AppTheme.duoPurple : Colors.grey[300]!,
-                    width: 3,
-                    style: isUnlocked ? BorderStyle.solid : BorderStyle.solid,
-                  ),
-                  boxShadow: isUnlocked
-                      ? [
-                          BoxShadow(
-                            color: AppTheme.duoPurple.withValues(alpha: 0.15),
-                            blurRadius: 12,
-                            offset: const Offset(0, 4),
-                          ),
-                        ]
-                      : null,
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+              width: 72,
+              height: 72,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isUnlocked ? Colors.white : Colors.grey[50]!,
+                border: Border.all(
+                  color: isUnlocked
+                      ? AppTheme.duoPurple.withValues(alpha: 0.8)
+                      : Colors.grey[200]!,
+                  width: isNew ? 3.0 : 2.5,
                 ),
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: [
+                boxShadow: isUnlocked
+                    ? [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.05),
+                          blurRadius: 10,
+                          offset: const Offset(0, 4),
+                        ),
+                      ]
+                    : null,
+              ),
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
                     Center(
                       child: ColorFiltered(
                         colorFilter: isUnlocked
@@ -92,10 +94,10 @@ class DuoBadge extends StatelessWidget {
                     ),
                     if (!isUnlocked)
                       Positioned(
-                        bottom: -2,
-                        right: -2,
+                        bottom: -4,
+                        right: -4,
                         child: Container(
-                          padding: const EdgeInsets.all(4),
+                          padding: const EdgeInsets.all(5),
                           decoration: BoxDecoration(
                             color: Colors.white,
                             shape: BoxShape.circle,
@@ -103,58 +105,58 @@ class DuoBadge extends StatelessWidget {
                               color: Colors.grey[300]!,
                               width: 2,
                             ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.1),
+                                blurRadius: 4,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
                           ),
                           child: Icon(
                             Icons.lock_rounded,
                             color: Colors.grey[400],
-                            size: 14,
+                            size: 12,
                           ),
                         ),
                       ),
                     if (isNew)
                       Positioned(
-                        top: -4,
-                        right: -4,
-                        child:
-                            Container(
-                                  width: 22,
-                                  height: 22,
-                                  decoration: const BoxDecoration(
-                                    color: AppTheme.duoRed,
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: const Center(
-                                    child: Text(
-                                      '!',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ),
-                                )
-                                .animate(onPlay: (c) => c.repeat())
-                                .scale(
-                                  duration: 1000.ms,
-                                  begin: const Offset(0.8, 0.8),
-                                  end: const Offset(1.1, 1.1),
-                                )
-                                .then()
-                                .scale(
-                                  duration: 1000.ms,
-                                  begin: const Offset(1.1, 1.1),
-                                  end: const Offset(0.8, 0.8),
-                                ),
+                        top: 2,
+                        right: 2,
+                        child: Container(
+                          width: 14,
+                          height: 14,
+                          decoration: BoxDecoration(
+                            color: AppTheme.duoRed,
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.white, width: 2),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.1),
+                                blurRadius: 4,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                        )
+                            .animate(onPlay: (c) => c.repeat(reverse: true))
+                            .scale(
+                              duration: 1.seconds,
+                              begin: const Offset(0.8, 0.8),
+                              end: const Offset(1.1, 1.1),
+                              curve: Curves.easeInOut,
+                            ),
                       ),
                   ],
-                ),
-              )
+              ),
+            ),
+          )
               .animate(onPlay: (c) => isNew ? c.repeat() : null)
-              .shimmer(duration: isNew ? 2.seconds : Duration.zero, angle: -0.5)
+              .shimmer(duration: 2.seconds, angle: -0.5, color: Colors.white24)
               .animate()
               .scale(
-                begin: const Offset(0.9, 0.9),
+                begin: const Offset(0.95, 0.95),
                 end: const Offset(1.0, 1.0),
                 duration: 400.ms,
                 curve: Curves.elasticOut,

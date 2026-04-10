1345473e1e7bb543fa25a7298bf7c06fa15a453e
Leo Wong <gnowoel@gmail.com>
2026-04-10 15:57:36 +0800

    test: cleanup seeding test imports

--- talktive_server/test/integration/seeding_test.dart
+++ talktive_server/test/integration/seeding_test.dart
@@ -1,4 +1,3 @@
-import 'package:serverpod/serverpod.dart';
 import 'package:test/test.dart';
 import 'package:talktive_server/src/generated/protocol.dart';
 import 'test_tools/serverpod_test_tools.dart';
